e59df94d6cd7edf5d8dd988e12cb7b8c5e8a3c20
Marc Vergara <marc.vergara.f@gmail.com>
2017-09-21 19:57:11 +0200

    added github

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -8,6 +8,7 @@ const Contact = () => {
 		<h1><FontAwesome className={style.faStack} name='address-card'/>Contact Me</h1>
 		<h3>Mobile: +34 682 04 32 89</h3>
 		<h3>Email: marc.vergara.f@gmail.com</h3>
+		<h3>Github: www.github.com/femave</h3>
 		</div>
 		)
 }
